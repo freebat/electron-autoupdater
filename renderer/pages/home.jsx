871f1cbe6a46eee2cf8260b185a52dd765a16658
batkhuubat <freebat2003@yahoo.com>
2023-09-14 09:13:38 +0800

    u

--- renderer/pages/home.jsx
+++ renderer/pages/home.jsx
@@ -5,40 +5,10 @@ import Link from 'next/link';
 import child_process from "child_process";
 import * as util from "util";
 import sudoer from "sudo-prompt";
-import path from "path";
-import { KillSwitch } from "./killswitch";
-
+import path from "path"; 
 
 function Home() {
-
-  const killnetwork = new KillSwitch();
-
-  async function command(AddVPNCmd) {
-    return new Promise((resolve, reject) => {
-      //  const childProcess = spawn('powershell.exe', ['-Command', 'Get-VpnConnection -Name "Your VPN Connection Name" | Select-Object -ExpandProperty ConnectionStatus']);
-      const result_add_vpn = child_process.spawn('powershell.exe', ['-Command', AddVPNCmd]);     
-     
-      let errorMessage_out='';
-      let result = '';
-      result_add_vpn.stdout.on('data', (data) => {
-        result = 'success';
-      });
-  
-      result_add_vpn.stderr.on('data', (data) => {
-        errorMessage_out += data.toString();
-        result='error';
-      });
-  
-      result_add_vpn.on('close', (code) => { 
-        console.log(errorMessage_out);
-        if (code === 0) {
-          resolve(result);
-        } else {
-          reject(result);
-        }
-      });
-    });
-  }
+ 
  
   async function handleClick() {
     console.log("increment like asdasd count")
@@ -46,10 +16,7 @@ function Home() {
     var path = require('path');
     var p = path.join(__dirname, '.', 'README.md');
     console.log(p);
-    
-    killnetwork.enabled_test();
-
- 
+   
 
  
   /*  await command(ipSecConf).then((value) => {       
@@ -60,26 +27,7 @@ function Home() {
        });  
 */
   }
-  async function disabled_handleClick() {
-    console.log("increment like asdasd count")
-
-    var path = require('path');
-    var p = path.join(__dirname, '.', 'README.md');
-    console.log(p);
-    
-    killnetwork.disable_test();
-
  
-
- 
-  /*  await command(ipSecConf).then((value) => {       
-      console.log(value)  
-      //    child_process.spawn('rasdial', [`${connectionName}`, '', '']);
-       }).catch(error=>{
-        console.log(error, 'Settings error has occurred');
-       });  
-*/
-  }
 
   return (
     <React.Fragment>
@@ -92,9 +40,7 @@ function Home() {
           <Link href="/next">
             <a>Go tod next page</a>
           </Link>
-          <button onClick={handleClick}>Enabled</button>
-
-          <button onClick={disabled_handleClick}>Disabled</button>
+        
         </p>
         <img src="/images/logo.png" />
       </div>
